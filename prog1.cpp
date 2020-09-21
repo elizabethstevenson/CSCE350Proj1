374448badb881999c44f3186e31c653413fbeb82
elizabethstevenson <60235792+elizabethstevenson@users.noreply.github.com>
2020-09-20 18:28:56 -0700

    yall ....

--- prog1.cpp
+++ prog1.cpp
@@ -12,7 +12,7 @@ using std::cin;
 using std::endl;
 using std::string;
 using std::stringstream;
-using std::stack;
+using std::stack; //I tried to do it with stacks first but then it got a little too confusing so I switched to vectors
 using std::vector;
 using std::move;
 
@@ -33,7 +33,8 @@ vector<string> peg2;
 vector<string> pegTemp;
 
 //prints which certain disk moved from certain peg to certain peg
-void printStep(string n, int from, int to) {
+//n was supposed to be a string but it would not work well in moveStackOfPairs and solveHuger
+void printStep(int n, int from, int to) {
     stepCounter++;
     cout << "Step " << stepCounter << ": Move disk " << n << " from peg " << from << " to peg " << to << ".";
     cout << endl;
@@ -60,133 +61,104 @@ void printPegStatus() {
     cout << endl;
 }
 
-void moveStackOfPairs(string n, int a, int b, int c) {
-    if(n.length()<2) {
-        cout << "APPLE ";
-
-
+/* I originally made the variable n a string so then the disks could 
+ * go through the moves from peg to peg but it would not compile 
+ * and would not show the labels for the pegs so I changed it back
+ * to int to show that. The comments behind the recursion were code that 
+ * I was trying to do when n was a string and trying to go through each character.
+ * I was trying a lot of different approaches with the recurions with the string
+ * but all the trials led to errors.
+*/
+void moveStackOfPairs(int n, int a, int b, int c) {
+    if(n<2) {
         //move a disk from A to C
         printStep(n, a, c);
-        //MOVE PEG
-        peg2.end() = move(peg0.end());
-        //move(peg0.end(), peg2.end());
+        peg2.end() = move(peg0.end()); //move(peg0.end(), peg2.end());
         printPegStatus();
 
 
         //move a disk from A to B
         printStep(n, a, b);
-        //MOVE PEG
-        peg1.end() = move(peg0.end());
-        //move(peg0.end(), peg1.end());
+        peg1.end() = move(peg0.end()); //move(peg0.end(), peg1.end());
         printPegStatus();
 
 
         //move a disk from C to B
         printStep(n, c, b);
-        //MOVE PEG
-        peg2.end() = move(peg1.end());
-        //move(peg2.end(), peg1.end());
+        peg2.end() = move(peg1.end()); //move(peg2.end(), peg1.end());
         printPegStatus();
-
     } else {
-        cout << "BANANA ";
-
-
-        moveStackOfPairs(n.at(pegTemp.size()-1), a, b, c);
+        moveStackOfPairs(n-1, a, b, c); //moveStackOfPairs(n.at(pegTemp.size()-1), a, b, c);
         
-
         //move a disk from A to C
         printStep(n, a, c);
-        //MOVE PEG
-        peg2.end() = move(peg0.end());
-        //move(peg0.end(), peg2.end());
+        peg2.end() = move(peg0.end()); //move(peg0.end(), peg2.end());
         printPegStatus();
         
-
         //move another disk from A to C
         printStep(n, a, c);
-        //MOVE PEG
-        peg2.end() = move(peg0.end());
-        //move(peg0.end(), peg2.end());
+        peg2.end() = move(peg0.end()); //move(peg0.end(), peg2.end());
         printPegStatus();
 
-
-        moveStackOfPairs(n.at(pegTemp.size()-1), b, a, c);
-
+        moveStackOfPairs(n-1, a, b, c); //moveStackOfPairs(n.at(pegTemp.size()-1), b, a, c);
 
         //move a disk from C to B
         printStep(n, c, b);
-        //MOVE PEG
-        peg1.end() = move(peg2.end());
-        //move(peg2.end(), peg1.end());
+        peg1.end() = move(peg2.end()); //move(peg2.end(), peg1.end());
         printPegStatus();
-
 
         //move another disk from C to B
         printStep(n, c, b);
-        //MOVE PEG
-        peg1.end() = move(peg2.end());
-        //move(peg2.end(), peg1.end());
+        peg1.end() = move(peg2.end()); //move(peg2.end(), peg1.end());
         printPegStatus();
 
-
-        moveStackOfPairs(n.at(pegTemp.size()-1), a, b, c);
+        moveStackOfPairs(n, a, b, c); //moveStackOfPairs(n.at(pegTemp.size()-1), a, b, c);
     }
 }
 
-void solveHuger(string n, int a, int b, int c) {
-    if(n.length()<2) {
-        cout << "ORANGE ";
-
-
+/* I also originally made the variable n a string so then the disks could 
+ * go through the moves from peg to peg but it would not compile 
+ * and would not show the labels for the pegs so I changed it back
+ * to int to show that.  The comments behind the recursion were code that 
+ * I was trying to do when n was a string and trying to go through each character.
+ * I was trying a lot of different approaches with the recurions with the string
+ * but all the trials led to errors.
+*/
+void solveHuger(int n, int a, int b, int c) {
+    if(n<2) {
         //move a disk from A to C
         printStep(n, a, c);
-        //MOVE PEG
-        peg2.end() = move(peg0.end());
-        //move(peg0.end(), peg2.end());
+        peg2.end() = move(peg0.end()); //move(peg0.end(), peg2.end());
         printPegStatus();
 
 
         //move a disk from A to B
         printStep(n, a, b);
-        //MOVE PEG
-        peg1.end() = move(peg0.end());
-        //move(peg0.end(), peg1.end());
+        peg1.end() = move(peg0.end()); //move(peg0.end(), peg1.end());
         printPegStatus();
-
     } else {
-        cout << "GRAPE ";
-
-        moveStackOfPairs(n.at(pegTemp.size()-1), a, b, c);
+        moveStackOfPairs(n-1, a, b, c); //moveStackOfPairs(n.at(pegTemp.size()-1), a, b, c);
 
         //move a disk from A to C
         printStep(n, a, c);
-        //MOVE PEG
-        peg2.end() = move(peg0.end());
-        //move(peg0.end(), peg2.end());
+        peg2.end() = move(peg0.end()); //move(peg0.end(), peg2.end());
         printPegStatus();
 
 
         //move a disk from A to B
         printStep(n, a, b);
-        //MOVE PEG
-        peg1.end() = move(peg0.end());
-        //move(peg0.end(), peg1.end());
+        peg1.end() = move(peg0.end()); //move(peg0.end(), peg1.end());
         printPegStatus();
 
-
-        moveStackOfPairs(n.at(pegTemp.size()-1), a, b, c);
+        moveStackOfPairs(n-1, a, b, c); //moveStackOfPairs(n.at(pegTemp.size()-1), a, b, c);
 
         
         //move disk from C to B
         printStep(n, c, b);
-        //MOVE PEG
-        peg1.end() = move(peg2.end());
-        //move(peg2.end(), peg1.end());
+        peg1.end() = move(peg2.end()); //move(peg2.end(), peg1.end());
         printPegStatus();
 
-
-        solveHuger(n.at(pegTemp.size()-1), a, b, c);
+        solveHuger(n-1, a, b, c); //solveHuger(n.at(pegTemp.size()-1), a, b, c);
     }
 }
 
@@ -198,7 +170,7 @@ int main(int argc, char* argv[]) {
     int b = 1;
     int c = 2;
 
-    //making the stack/first peg populated with A1B2C3...
+    //making the vector/first peg populated with A1B2C3...
     for(int i = 0; i < n; i++) {
         char addLetter = i + 'A';
         char addNumber = i + '1';
@@ -217,19 +189,20 @@ int main(int argc, char* argv[]) {
         peg0.push_back(number);
     }
 
+    //making a string with A1B2C3...
     string tower;
     for(unsigned int i = 0; i < pegTemp.size(); ++i){
         tower += pegTemp.at(i);
     }
-    //reverse(tower.begin(), tower.end());
-
 
     while(cin.good()) { 
         cout << "Starting At:" << endl;
         printPegStatus();
-        solveHuger(tower, a, b, c);
+        //solveHuger(tower, a, b, c); this was for when the first parameter was a string
+        solveHuger(n, a, b, c);
         cout << "Done!" << endl;
         stepCounter = 0;
+        //clears pegs for the next input
         peg0.clear();
         peg1.clear();
         peg2.clear();
@@ -238,6 +211,3 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
-
-
-
